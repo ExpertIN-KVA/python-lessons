3c624c8a580fbbd9f5e322a15b69654b43042fe2
Tsalmas Anastasios <tsalmanastasios@gmail.com>
2025-01-13 19:12:03 +0200

    εκφωνήσεις ασκήσεων

--- 2024-2025/exercises/exercises_01/exercise_1-2.py
+++ 2024-2025/exercises/exercises_01/exercise_1-2.py
@@ -1,3 +1,14 @@
+# 1.2 Δίνονται οι παρακάτω μεταβλητές. Να χαρακτηριστούν ως string, int, float και boolean [1.3]
+# 1. Var1 = 10.89
+# 2. Var2 = ‘Python’
+# 3. Var3 = 215
+# 4. Var4 = 2/1
+# 5. Var5 = ‘true’
+# 6. Var6 = false
+# 7. Var7 = ‘-4’
+# 8. Var8 = 2 ** 2
+
+
 # 1. Double/Float
 # 2. String
 # 3. Integer

d57e6b5f1c55b019f1005fa44e98fd76e3d15fc7
Tsalmas Anastasios <tsalmanastasios@gmail.com>
2025-01-16 19:44:32 +0200

    changes in exercise 2.4

--- 2024-2025/exercises/exercises_02/exercise_2-4.py
+++ 2024-2025/exercises/exercises_02/exercise_2-4.py
@@ -6,18 +6,16 @@
 
 kilometers = int(input("Δώσε τα χιλιόμετρα του αυτοκινήτου: "))
 
+
 next_service_category = (kilometers // 10000 + 1) * 10000
 
-print(
-    "Το επόμενο service θα γίνει σε "
-    + next_service_category
-    - kilometers
-    + " χιλιόμετρα"
-)
+difference = next_service_category - kilometers
+
+print("Το επόμενο service θα γίνει σε " + difference + " χιλιόμετρα")
 
 
-category = "Μεγάλο"
-if next_service_category % 20000 != 0:
-    category = "Μικρό"
+category = "Μικρό"
+if next_service_category % 20000 == 0:
+    category = "Μεγάλο"
 
 print("Το επόμενο service θα είναι '" + category + "'")
